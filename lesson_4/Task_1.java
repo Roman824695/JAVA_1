c780b9450356cab35c74d62b87561a03bdba8f28
Roman Tsykalo <u15921@mail.ru>
2023-03-16 15:34:18 +0400

    Исправлено.Добавил удаление строки

--- lesson_4/Task_1.java
+++ lesson_4/Task_1.java
@@ -2,6 +2,7 @@
 1.Реализовать консольное приложение, которое:
 Принимает от пользователя и “запоминает” строки.
 Если введено print, выводит строки так, чтобы последняя введенная была первой в списке, а первая - последней.  
+Если введено revert, удаляет предыдущую введенную строку из памяти.
 */
 
 package lesson_4;
@@ -15,12 +16,19 @@ public class Task_1 {
       public static void main(String[] args) {
         LinkedList<String> inputLL = new LinkedList<>();
         Scanner scanner = new Scanner(System.in);
-        String srtAdd;
+        String strAdd;
         System.out.println("Введите  строку.\n Для выхода используйте 'print'");
+        
 
-        while (!(srtAdd = scanner.nextLine()).equals("print"))
-            inputLL.addFirst(srtAdd);
-        scanner.close();
+        while (!(strAdd = scanner.nextLine()).equals("print")){
+            
+            if(strAdd.equals("revert")){
+                inputLL.remove(inputLL.get(0));
+            }else{
+                inputLL.addFirst(strAdd);
+        }
+        }
+        
         System.out.println(inputLL);
     }
     }
